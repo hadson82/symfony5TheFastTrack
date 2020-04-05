8b4d7c3dce1736fc2524194830798d90e7dfb230
Pavel <rogozhin.p@yandex.ru>
2020-04-05 12:29:39 +0300

    add twig and show homepage

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -6,25 +6,20 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
+use App\Repository\ConferenceRepository;
+use Twig\Environment;
 
 class ConferenceController extends AbstractController
 {
     /**
-     * @Route("/hello/{name}", name="homepage")
+     * @Route("/", name="homepage")
      */
-    public function index(string $name = '')
-    {
-        $greet = '';
-        if($name){
-            $greet = sprintf('<h1>Hello %s!</h1>', htmlspecialchars($name));
-        }
-        return new Response(<<<EOF
-                <html>
-                <body>
-                $greet
-                <img src="/images/under-construction.gif" />
-                </body>
-EOF
-            );
+    public function index(Environment $twig, ConferenceRepository $conferenceRepository){
+
+        return new Response($twig->render('conference/index.html.twig', [
+            'conferences' => $conferenceRepository->findAll(),
+        ]));
+
     }
+
 }
