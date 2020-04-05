9601e060e030ede764b3602537999ca0d727e20f
Pavel <rogozhin.p@yandex.ru>
2020-04-05 17:46:38 +0300

    create header

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -20,14 +20,11 @@ class ConferenceController extends AbstractController
     {
         $this->twig = $twig;
     }
+
     /**
      * @Route("/", name="homepage")
-     * @param Environment $twig
      * @param ConferenceRepository $conferenceRepository
      * @return Response
-     * @throws \Twig\Error\LoaderError
-     * @throws \Twig\Error\RuntimeError
-     * @throws \Twig\Error\SyntaxError
      */
     public function index(ConferenceRepository $conferenceRepository){
 
@@ -39,27 +36,42 @@ class ConferenceController extends AbstractController
 
     }
 
+//    /**
+//     * @Route("/conference/{id}", name="conference")
+//     * @param Request $request
+//     * @param Conference $conference
+//     * @param CommentRepository $commentRepository
+//     * @return Response
+//     */
+//    public function show(Request $request, Conference $conference, CommentRepository $commentRepository)
+//    {
+//        $offset = max(0, $request->query->getInt('offset', 0));
+//        $paginator = $commentRepository->getCommentPaginator($conference, $offset);
+//
+//        return new Response($this->twig->render('conference/show.html.twig', [
+//            'conference' => $conference,
+//            'comments' => $paginator,
+//            'previous' => $offset - CommentRepository::PAGINATOR_PER_PAGE,
+//            'next' => min(count($paginator), $offset + CommentRepository::PAGINATOR_PER_PAGE),
+//        ]));
+//    }
+
     /**
-     * @Route("/conference/{id}", name="conference")
-     * @param Request $request
+     * @Route("/conference/{slug}", name="conference")
      * @param Conference $conference
      * @param CommentRepository $commentRepository
+     * @param ConferenceRepository $conferenceRepository
      * @return Response
-     * @throws \Twig\Error\LoaderError
-     * @throws \Twig\Error\RuntimeError
-     * @throws \Twig\Error\SyntaxError
      */
-    public function show(Request $request, Conference $conference, CommentRepository $commentRepository)
+    public function show(Conference $conference, CommentRepository $commentRepository, ConferenceRepository $conferenceRepository)
     {
-        $offset = max(0, $request->query->getInt('offset', 0));
-        $paginator = $commentRepository->getCommentPaginator($conference, $offset);
 
         return new Response($this->twig->render('conference/show.html.twig', [
+            'conference' => $conferenceRepository->findAll(),
             'conference' => $conference,
-            'comments' => $paginator,
-            'previous' => $offset - CommentRepository::PAGINATOR_PER_PAGE,
-            'next' => min(count($paginator), $offset + CommentRepository::PAGINATOR_PER_PAGE),
+            'comments' => $commentRepository->findBy(['conference' => $conference], ['createdAt' => 'DESC']),
         ]));
     }
+
 
 }
