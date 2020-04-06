4324459da6269f6f48077549b65fdc7e884d1851
Pavel <rogozhin.p@yandex.ru>
2020-04-06 19:33:08 +0300

    basic authentification for admin done

---
+++ src/Migrations/Version20200406154326.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200406154326 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('ALTER TABLE admin ALTER roles TYPE VARCHAR(255)');
+        $this->addSql('ALTER TABLE admin ALTER roles DROP DEFAULT');
+        $this->addSql('ALTER TABLE admin ALTER roles SET NOT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('ALTER TABLE admin ALTER roles TYPE JSON');
+        $this->addSql('ALTER TABLE admin ALTER roles DROP DEFAULT');
+        $this->addSql('ALTER TABLE admin ALTER roles DROP NOT NULL');
+    }
+}
